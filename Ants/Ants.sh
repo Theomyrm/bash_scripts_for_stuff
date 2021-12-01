28cbb70a1b03f465fb43a174da46d322a571051b
T.Myrm <73954335+Theomyrm@users.noreply.github.com>
2021-12-01 02:05:41 -0600

    Ants Finalversion

--- Ants/Ants.sh
+++ Ants/Ants.sh
@@ -25,40 +25,41 @@
 # Presentacion: Texto que se imprime en pantalla
 echo "# -  #  -  #  -  #  -  #  -  #  -  #  -  #  -  #  -  #  -  #  -  #  -  #  -  #
 
-#  Hola, bienvenido a este buscador / explorador de hormigas. El objetivo    #
+#  Hola, bienvenido a este buscador de hormigas. Aqui podras buscar ya sea   #
 
-#  de este script es que, ya sea navegar a traves de las listas de hormigas  #
+#  por nombre de especie o nombre de genero a hormigas, de las que uno       #
 
-#  o al estar buscando una en particular, uno, desde terminal,  pueda tener  #
+#  quisiera sacar informacion genetica o en su defecto, descripciones de     #
 
-#  acceso a secuencias geneticas, descripciones, entre entre otras.          #
+#  articulos.                                                                #
 
 #  Disfruta.                                     Traido a todos por TMyrm    #
 
 # -  #  -  #  -  #  -  #  -  #  -  #  -  #  -  #  -  #  -  #  -  #  -  #  -  #
-
 "; sleep 2
 
 
 ## Variables a utilizar
-D1=0   # Variable para determinar presencia ausencia de dependencia EDirect
-D2=0   # Variable para determinar presencia ausencia de dependencia imagemagick
-ans="" # Variable para almacenar query
+D1=0    # Variable para determinar presencia ausencia de dependencia EDirect
+D2=0    # Variable para determinar presencia ausencia de dependencia imagemagick
+ans=""  # Variable para almacenar query
+arr=(0) # Array para almacenar opciones
 
 echo "Revisando dependencias:
 " && sleep 1
 
-## Menu de opciones:
-function menu() {
-  echo "
-  ¿Que funcion necesitas?:
-        1 Explorador
-        2 Buscador"
-  read answ2
-  case $answ2 in
-    1 ) explorador ;;
-    2 ) buscador ;;
-  esac
+function informacion(){
+  echo -e "  ## - ## - ## - ## - ## - ## - ## - ## - ## - ## - ## - ## - ## - ## - ## - ##
+  ##                        Sobre script ANTS:                               ##
+  ##                                                                         ##
+  ##    Este script fue elaborado por Elrik Miralda como parte de la         ##
+  ##    asignatura de bioinformatica, de la Universidad Autonoma de          ##
+  ##    Yucatan. La intencion inicial era tener acceso tanto a fotografias   ##
+  ##    como informacion genetica, pero la API de webant se encuentra abajo  ##
+  ##    Asi que quedo simplemente como un buscador. Espero sea de su agrado  ##
+  ##                                                                         ##
+  ## - ## - ## - ## - ## - ## - ## - ## - ## - ## - ## - ## - ## - ## - ## - ##\n\n"
+exit 0;
 }
 
 ## Revision de dependencias
@@ -71,7 +72,7 @@ function dependency() {
       echo "eDirect encontrado" # No es necesario instalar
   else
       echo "No se encontro Edirect" # Es necesario instalar
-      D1=1; return D1
+      D1=1;
   fi
 
   # Revisar existencia de la dependencia ImageMagick
@@ -79,7 +80,7 @@ function dependency() {
     echo 'ImageMagick encontrado'
   else
     echo 'No se encontro ImageMagick'
-    D2=1; return D2
+    D2=1;
   fi
 
 #  -  -  #  -  -  #  -  -  Seccion instalacion  -  -  #  -  -  #  -  -  #
@@ -138,145 +139,84 @@ function dependency() {
         menu
       fi
 }
-dependency ## <---- Inicio del script
-
 ## Funcion para carga de datos en R y __main__
 
-function Rscr(){
-  ## Area de funciones de R y variables en general
-    ## Funciones almacenadas en strings:
-      ## Base; carga de archivo, creacion DB y seleccion:
+###                                                                       ###
+## Area de funciones de R y variables en general
+  ## Funciones almacenadas en strings:
+    ## Base; carga de archivo, creacion DB y seleccion:
 
-  #   #       #       #       Carga de datos a R       #       #        #   #
+#   #       #       #       Carga de datos a R       #       #        #   #
 
-  ## Funcion para cargar los datos basicos de la base de datos de          ##
-  ## hormigas. Tambien se encarga de generar las bases de datos utilizadas ##
-  ## para realizar las busquedas y navegaciones.                           ##
+## Funcion para cargar los datos basicos de la base de datos de          ##
+## hormigas. Tambien se encarga de generar las bases de datos utilizadas ##
+## para realizar las busquedas y navegaciones.                           ##
 
-  base="user <- Sys.info()[["user"]]      ## Lee usuario para nombre de home/user
-  wdhome <- paste("//home//",user,sep = "")   ## Genera ~/
-  wd <- paste(wdhome,"//Ants//DB//",sep = "") ## Genera ~/Ants/DB
-  setwd(wd) ## set DB as WorkingDirectory -> /home/user/Ants/DB
-  ##
-  DB <- read.csv('ANTDB.csv', header = T, sep = ',') ## Carga base de datos
-  "
+#       #       #       #        #        #         #       #       #
+
+## Funcion que da la opcion de buscar informacion de hormigas por genero o especie
+function buscador() {
+  echo -e "Procure ser lo mas preciso que pueda con sus busquedas:"; sleep 1;
+  echo -e "Las busquedas deben utilizar o nombre de especie y genero (ej: Atta cephalotes)"; sleep 1;
+  echo -e "O en su defecto, solo contener nombre de genero (ej: Acromyrmex)\n"; sleep 1;
+  echo -e "inserte su busqueda"
+  read que
+  ry='"'
+  query="${ry}^${que}${ry}" ## <-- Sirve para usar formato de busqueda de grep->R
+
+  #       #       #       Variables de buscador       #       #       #
+
+  ## Funcion para realizar busqueda por nombre puntual.Especie o genero ##
+  ## Tambien determina si se esta realizando una busqueda por especie   ##
+  ## o por genero. Donde, en caso de ser por especie, se realiza la     ##
+  ## busqueda directa y en caso de ser por genero, se muestra una lista ##
+  ## de posibilidades a elegir.                                         ##
+
   #       #       #       #        #        #         #       #       #
 
-  ## Funcion que da la opcion de explorar las hormigas por genero y especie
-  function explorador() {
-    #       #       #       Variables de explorador       #       #       #
-
-    ## Funcion para realizar exploraciones de generos y especies.         ##
-    ## Una vez seleccionada la especie, se realizara query en eDirect     ##
-
-    ## Comando de R para generar lista de generos
-    giveNavList="query <- unique(data.frame(DB$Raw.genus))
-    names(query) <- NULL
-    write.table(query, file="//home//user//Ants//DB//result.txt", sep = "")"
-
-    giveAlphList="query <- unique(data.frame(DB$Raw.genus[grep(pattern= "^${letra}", DB$Raw.genus)]))
-    names(query) <- NULL
-    write.table(query, file="//home//user//Ants//DB//result.txt", sep = "")"
-
-    echo -e "Desea ver la lista completa de generos a elegir o prefiere por indice alfabetico?"
-    echo -e "\n1. Lista completa\n2. Por Indice alfabetico"
-    read nav1
-
-    case $nav1 in
-      # Muestra lista completa de generos.
-      1. )  ;; R --slave <<EOF $base $giveNavList EOF && more ~/Ants/DB/result.txt
-      # Muestra generos por letra escogida
-      2. )  ;; echo -e "Inserte la letra deseada"&& read letra && R --slave <<EOF $base $giveAlphList EOF && more ~/Ants/DB/result.txt
-      # Cierra el programa
-      * ) exit 0 ;;
-    esac
-
-    #       #       #       #        #        #         #       #       #
-    #       #       #        Lector de Indices          #       #       #
-    ## Lee, despliega resultados y los muestra como opciones a elegir  ##
-
-    if [ -s '~/Ants/DB/result.txt' ] then
-      echo -e "Selecciona por numero tu eleccion\n"; sleep 2
-      arr=(0)
-      i=1
-      while IFS= read -r line; do
-        dat="${i}_${line}"
-        echo $dat >> BashOut ## OUT ----->
-        arr+=(${dat})
-        i=$((i+1))
-      done < "result.txt" | cut -d '"' -f4;
-      more BashOut
-      echo -e "\nNumero:"
-      sleep 2
-      read ans ## Lee la respuesta
-      rm BashOut ## DEL <------
-    fi
-    Equery
-
-
-  }
-
-  ## Funcion que da la opcion de buscar informacion de hormigas por genero o especie
-  function buscador() {
-    echo -e "Procure ser lo mas preciso que pueda con sus busquedas:"; sleep 1;
-    echo -e "Las busquedas deben utilizar o nombre de especie y genero (ej: Atta cephalotes)"; sleep 1;
-    echo -e "O en su defecto, solo contener nombre de genero (ej: Acromyrmex\n)"; sleep 1;
-    echo -e "inserte su busqueda"
-    read query
-
-    #       #       #       Variables de buscador       #       #       #
-
-    ## Funcion para realizar busqueda por nombre puntual.Especie o genero ##
-    ## Tambien determina si se esta realizando una busqueda por especie   ##
-    ## o por genero. Donde, en caso de ser por especie, se realiza la     ##
-    ## busqueda directa y en caso de ser por genero, se muestra una lista ##
-    ## de posibilidades a elegir.                                         ##
-
-    giveQueryList="query <- unique(data.frame(DB$Raw.species[grep(pattern= "^${query}", DB$Raw.species)]))
-    names(query) <- NULL
-    write.table(query, file="//home//user//Ants//DB//result.txt", sep = "")
+  #       #       #        Inicio de busqueda R       #       #       #
+  R --slave <<EOF
+      DB <- read.csv('~//Ants//DB//ANTDB.csv', header = T, sep = ',') ### Carga base de datos
+      attach(DB)
+      query <- unique(data.frame(species[grep(pattern= ${query}, species)]))
+      names(query) <- NULL
+      write.table(query, file="~//Ants//DB//result.txt", sep = "")
+EOF
     ## Genera dataframe con query y wildcard de patron
-    ## divide segun resultado sea especie o genero"
+    ## divide segun resultado sea especie o genero
 
-    #       #       #       #        #        #         #       #       #
+  #       #       #       #        #        #         #       #       #
+  #       #       #        Lector de Indices          #       #       #
+  ## Lee, despliega resultados y los muestra como opciones a elegir  ##
 
-    #       #       #        Inicio de busqueda R       #       #       #
-      R --slave <<EOF
-        $base ### Carga base de datos
-        $giveQueryList ### Realiza busqueda
-        EOF
-    #       #       #       #        #        #         #       #       #
-    #       #       #        Lector de Indices          #       #       #
-    ## Lee, despliega resultados y los muestra como opciones a elegir  ##
+  echo -e "Selecciona tu hormiga de eleccion (Por nombre completo)\n"; sleep 2
 
-    if [ -s 'result.txt' ] then
-  echo -e "Selecciona por numero tu eleccion\n"; sleep 2
-  arr=(0)
   i=1
   while IFS= read -r line; do
-   dat="${i}_${line}"
-   echo $dat >> BashOut ## OUT ----->
-   arr+=(${dat})
-   i=$((i+1))
- done < "result.txt" | cut -d '"' -f4;
-  more BashOut
-  echo -e "\nNumero:"
+    dat="${line}"
+    echo $dat >> ~/Ants/DB/BashOut ## OUT ----->
+    arr+=(${line})
+    i=$((i+1))
+  done < ~/Ants/DB/result.txt | cut -d '"' -f4;
+  more ~/Ants/DB/BashOut
+  echo -e "\nNombre completo de especie:"
   sleep 2
   read ans ## Lee la respuesta
-  rm BashOut ## DEL <------
-fi
-Equery
-  }
+  rm ~/Ants/DB/BashOut
+  rm ~/Ants/DB/result.txt
+  Equery
+
 }
 
 function Equery() {
+ ###                                                        ###
   ## Primero, despliega informacion taxonomica generica:
-  echo -e "El organismo que escogiste fue:"
-  esearch -db taxonomy -query "$ans" | efetch -format abstract
+  echo -e "El organismo que escogiste fue:\n"
+  esearch -db taxonomy -query "${ans}" | efetch -format abstract
   sleep 2 ## Para ajustar el ritmo del texto del programa
 
   ## Ahora, pregunta al usuario que es lo que necesita:
-  echo -e "Que es lo que deseas encontrar sobre el organismo?\n"
+  echo -e "Que es lo que deseas encontrar sobre el organismo? (Inserte numero)\n"
   echo -e "
   1. Secuencias geneticas
   2. Encontrar articulos
@@ -285,13 +225,29 @@ function Equery() {
 
   case $info in
     # Muestra secuencias geneticas.
-    1. ) echo -e "inserta el numero de lineas de texto que quieres ver" && read numQg &&
-    esearch -db gene -blast "$ans" | efetch -format fasta | head -n $numQg ;;
+    1 ) echo -e "inserta el numero de lineas de texto que quieres ver" && read numQg &&
+    esearch -db nucleotide -query "${ans}" | efetch -format fasta | head -n $numQg ;;
     # Muestra informacion sobre especie
-    2. ) echo -e "inserta el numero de lineas de texto que quieres ver" && read numQa &&
-    esearch -db pubmed -query "$ans" | efetch -format abstract | head -n numQa  ;;
+    2 ) echo -e "inserta el numero de lineas de texto que quieres ver" && read numQa &&
+    esearch -db pubmed -query "$ans" | efetch -format abstract | head -n ${numQa}  ;;
     # Cierra el programa
     * ) exit 0 ;;
   esac
 
-  exit 0}
+  exit 0;}
+ ###                                                        ###
+
+ ## Menu de opciones:
+function menu() {
+ echo "
+  ¿Que funcion necesitas? (Inserte numero):
+       1 Buscador
+       2 Informacion sobre script"
+ read answ2
+  case $answ2 in
+   1 ) buscador ;;
+   2 ) informacion ;;
+  esac
+ }
+dependency ## <---- Inicio del script
+exit 0;
--- Ants/R/AntDB.R
+++
@@ -1,5 +0,0 @@
-"""
-Script complementario para la creacion y creacion de bases de datos temporales
-Por tmyrm:
-
-"""
